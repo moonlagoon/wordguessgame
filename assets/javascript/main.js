ca48f205946683c60994e15ee7d64a23a29056c8
moonlagoon <courtneyroutsong@gmail.com>
2018-12-11 11:40:02 -0500

    12112018

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,55 @@
 var wordList = [
     "pikachu",
     "sky",
-    "coding"
+    "coding",
+    "cat",
   ];
 
-  let random = Math.floor(Math.random()*wordList.length);
-  console.log(wordList[0])
+  var answer = "";
+
+  // grabbing random word from wordList
+
+  let random = wordList[Math.floor(Math.random()*wordList.length)];
+  console.log(random)
+
+ document.getElementById("word").innerHTML = "";
+
+ // grabbing from html
+
+for (let i = 0; i < random.length; i++) {
+  document.getElementById("word").innerHTML += "_";
+}
+
+// everytime you push key it registers
+
+document.onkeyup = function(event) {
+  let userInput = event.key;
+ 
+for (let i = 0; i < random.length; i++) {
+  
+  if(random[i].charAt() == userInput) {
+  // replaces underscore with key inputted if correct
+    answer = document.getElementById("word").innerHTML;
+    answer = answer.replaceAt(i , userInput);
+    console.log(answer)
+    document.getElementById("word").innerHTML = answer;
+
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+  
+};
+
+String.prototype.replaceAt=function(index, replacement) {
+  return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+}
