96bd30942f26a601b348592ca59ecb03d8651c12
moonlagoon <courtneyroutsong@gmail.com>
2018-12-13 22:48:32 -0500

    i tried

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,17 @@
-var wordList = [
+const wordList = [
     "pikachu",
     "sky",
     "coding",
     "cat",
+    "christmas",
+    "guitar",
+    "apple"
   ];
 
-  var answer = "";
+  let wins = 0,
+    losses = 0
+
+  var placeHolder = "";
 
   // grabbing random word from wordList
 
@@ -14,7 +20,7 @@ var wordList = [
 
  document.getElementById("word").innerHTML = "";
 
- // grabbing from html
+ // grabbing _ space from html and changes in size based on random word
 
 for (let i = 0; i < random.length; i++) {
   document.getElementById("word").innerHTML += "_";
@@ -29,14 +35,41 @@ for (let i = 0; i < random.length; i++) {
   
   if(random[i].charAt() == userInput) {
   // replaces underscore with key inputted if correct
-    answer = document.getElementById("word").innerHTML;
-    answer = answer.replaceAt(i , userInput);
-    console.log(answer)
-    document.getElementById("word").innerHTML = answer;
+    placeHolder = document.getElementById("word").innerHTML;
+    placeHolder = placeHolder.replaceAt(i , userInput);
+    console.log(placeHolder)
+    document.getElementById("word").innerHTML = placeHolder;
 
   }
   
 }
+
+// lists all letters user types in
+
+
+
+
+
+// changes and tells the amount of characters in each word
+
+
+
+
+
+
+// how many turns you have left
+
+
+
+
+
+
+// if the players wins
+
+
+
+
+// if the player loses
 
 
 
